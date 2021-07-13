1a5acbec6547ce120c197c96b1d107324b24d477
Luis <luistorresespitia98_@hotmail.com>
2021-07-12 20:59:23 -0500

    se agrego components y services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateEmployeeComponent,
+    NavbarComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule,
