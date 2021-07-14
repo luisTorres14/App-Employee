26d359cbb3f7249b6b19844e639b61a42bb4912f
Luis <luistorresespitia98_@hotmail.com>
2021-07-13 21:10:57 -0500

    agregando cuerpo a la app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listEmployee', pathMatch: 'full' },
+  { path: 'listEmployee', component: ListEmployeeComponent },
+  { path: 'createEmployee', component: CreateEmployeeComponent },
+  { path: '**', redirectTo: 'listEmployee', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
